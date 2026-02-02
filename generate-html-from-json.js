093a2f5c7f9fda5b036886b704f388828ef4b9ec
Frédéric Savard <fredx21@hotmail.com>
2026-02-02 13:26:05 -0500

    Ajout de member-template

--- generate-html-from-json.js
+++ generate-html-from-json.js
@@ -1,6 +1,6 @@
 /**
  * Script de génération des pages HTML des membres à partir des fichiers JSON
- * Remplace l'ancien système XSL/XML
+ * Remplace l'ancien système XSL/XML en utilisant un template HTML
  */
 
 const fs = require('fs');
@@ -10,6 +10,7 @@ const path = require('path');
 const CONFIG = {
   jsonDir: path.join(__dirname, 'output', 'members-json'),
   outputDir: path.join(__dirname, 'docs'),
+  templatePath: path.join(__dirname, 'templates', 'member-template.html'),
   currentYear: new Date().getFullYear()
 };
 
@@ -114,88 +115,36 @@ function generateSections(pageData) {
 }
 
 /**
- * Génère le HTML complet d'une page membre
+ * Génère le HTML complet d'une page membre en utilisant le template
  * @param {Object} member - Données du membre
  * @param {Object} language - Données de la langue courante
  * @param {Array} allLanguages - Toutes les langues disponibles
+ * @param {string} template - Contenu du template HTML
  * @returns {string} - HTML complet de la page
  */
-function generateMemberPage(member, language, allLanguages) {
-  const languageLinks = generateLanguageLinks(allLanguages, language.code, member.pageName);
-  const contactInfo = generateContactInfo(member);
-  const sections = generateSections(language.page);
+function generateMemberPage(member, language, allLanguages, template) {
+  // Remplacer tous les placeholders dans le template
+  let html = template;
 
-  return `<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-  <meta name="author" content="Frederic Savard" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-  <title>Clinique de psychologie Urbania</title>
-  <link rel="stylesheet" href="css/ui.css" />
-  <link rel="stylesheet" href="css/style.css" />
-  <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-40245323-1', 'psychologie-urbania.com');
-    ga('send', 'pageview');
-  </script>
-</head>
-<body>
-  <div class="introContainer">
-    <div id="intro">
-      <div class="logo">Clinique de Psychologie Urbania</div>
-      <div class="navigationHeader">
-        <p><span><a href="./">accueil</a><a class="currentpage" href="team">équipe</a><a href="services">services</a><a href="career">carrière</a><a href="contact">contact</a><a href="links">liens</a></span></p>
-      </div>
-    </div>
-  </div>
-  <div class="mainContainer">
-    <div class="content">
-      <div class="header">
-        ${languageLinks}
-        <h3>
-          <span>${member.name}, <span>${language.academic}</span></span>
-        </h3>
-      </div>
-      <div class="cf">
-        <div class="col2_66_33">
-          <div class="section cf"><div class="memberPhotoWrap"><div class="memberPhoto" style="background-image:url('${member.photo}')"></div></div></div>
-          <div class="section cf">
-            <div id="contactus">
-              <h1>
-                <span>Contact</span>
-              </h1>
-              ${contactInfo}
-            </div>
-          </div>
-        </div>
-        <div class="col1_66_33">
-          ${sections}
-          <hr/>
-          <div class="sectiontext warning">
-            NOTE: Les informations présentées sur cette page sont sous la responsabilité de leur auteur. La Clinique de psychologie Urbania n'intervient pas dans le contenu des pages personnelles ni dans la pratique des professionnels.
-          </div>
-        </div>
-      </div>
-      <hr/>
-      <div class="sectiontext copyright">
-        Copyright ${CONFIG.currentYear} © Clinique de Psychologie Urbania. Tous droits réservés.
-      </div>
-    </div>
-  </div>
-</body>
-</html>`;
+  html = html.replace(/{{NAME}}/g, member.name);
+  html = html.replace(/{{ACADEMIC}}/g, language.academic);
+  html = html.replace(/{{PHOTO}}/g, member.photo);
+  html = html.replace(/{{YEAR}}/g, CONFIG.currentYear);
+
+  // Remplacer les blocs dynamiques
+  html = html.replace('{{LANGUAGE_LINKS}}', generateLanguageLinks(allLanguages, language.code, member.pageName));
+  html = html.replace('{{CONTACT_INFO}}', generateContactInfo(member));
+  html = html.replace('{{SECTIONS}}', generateSections(language.page));
+
+  return html;
 }
 
 /**
  * Traite un fichier JSON de membre et génère les pages HTML
  * @param {string} filename - Nom du fichier JSON
+ * @param {string} template - Contenu du template HTML
  */
-function processMemberFile(filename) {
+function processMemberFile(filename, template) {
   const filepath = path.join(CONFIG.jsonDir, filename);
   const fileContent = fs.readFileSync(filepath, 'utf-8');
   const member = JSON.parse(fileContent);
@@ -204,7 +153,7 @@ function processMemberFile(filename) {
 
   // Générer une page pour chaque langue
   member.languages.forEach(language => {
-    const html = generateMemberPage(member, language, member.languages);
+    const html = generateMemberPage(member, language, member.languages, template);
 
     // Déterminer le nom du fichier de sortie
     const outputFilename = language.code === ''
@@ -233,6 +182,16 @@ function main() {
     fs.mkdirSync(CONFIG.outputDir, { recursive: true });
   }
 
+  // Charger le template
+  if (!fs.existsSync(CONFIG.templatePath)) {
+    console.error(`Erreur: Le template n'existe pas: ${CONFIG.templatePath}`);
+    process.exit(1);
+  }
+
+  const template = fs.readFileSync(CONFIG.templatePath, 'utf-8');
+  console.log(`Template chargé: ${CONFIG.templatePath}`);
+  console.log();
+
   // Lire tous les fichiers JSON
   const files = fs.readdirSync(CONFIG.jsonDir)
     .filter(file => file.endsWith('.json'));
@@ -243,7 +202,7 @@ function main() {
   // Traiter chaque fichier
   files.forEach(filename => {
     try {
-      processMemberFile(filename);
+      processMemberFile(filename, template);
     } catch (error) {
       console.error(`  ✗ Erreur lors du traitement de ${filename}:`, error.message);
     }
